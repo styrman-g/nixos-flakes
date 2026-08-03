da9c1ad69d7e713042e4b2f8de2eb6327de61155
Gustav Hogbring <72607904+styrman-g@users.noreply.github.com>
2026-08-03 17:46:24 +0200

    Add files via upload
    First upload of my nix flake

---
+++ flake.nix
@@ -0,0 +1,13 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+  outputs = { self, nixpkgs }: {
+    # replace 'joes-desktop' with your hostname here.
+    nixosConfigurations.thinkpad = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [ ./configuration.nix ];
+    };
+  };
+}
+
